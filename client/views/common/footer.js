8c5381c289dc74a50b3df09716f624d2556fd377
Nathan Clark <nclark@tegandigital.com>
2015-12-13 01:14:04 -0600

    Bootstrapped an auto complete feature

--- client/views/common/footer.js
+++ client/views/common/footer.js
@@ -5,3 +5,29 @@ Template.footer.rendered = function () {
     // $('.footer').addClass('fixed');
 
 };
+
+Template.footer.helpers({
+    settings: function() {
+        return {
+            position: "top",
+            limit: 5,
+            rules: [
+                {
+                    token: '@',
+                    collection: Channels,
+                    field: "name",
+                    template: Template.userPill
+                },
+                {
+                    token: '!',
+                    collection: Channels,
+                    field: "_id",
+                    options: '',
+                    matchAll: true,
+                    filter: { type: "autocomplete" },
+                    template: Template.dataPiece
+                }
+            ]
+        };
+    }
+});
